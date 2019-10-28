90edb714cccbcee8269bf78c588779bd6b067ae9
Denis Autushenka <dautushenka@tula.co>
2019-10-28 21:53:12 +0300

    practice2

--- main.go
+++ main.go
@@ -2,18 +2,43 @@ package main
 
 import (
 	"fmt"
-	"go-training/practice"
+	"go-training/practice1"
+	"go-training/practice2"
 )
 
-func main() {
+func runPractice1() {
 	// Practice 1
-	practice.Fillarray(10, "0");
-	a := practice.Filldiagonal(5, "1", "0");
+	practice1.Fillarray(10, "0");
+	a := practice1.Filldiagonal(5, "1", "0");
 	fmt.Println("[Practice 1] Print 2d array with filled dialonals")
-	practice.Print2darray(a);
+	practice1.Print2darray(a);
 
 	fmt.Println("")
 	// Practice 1.2
 	fmt.Println("[Practice 1.2] Print dice statistic")
-	practice.Dicestatistic(10000);
+	practice1.Dicestatistic(10000);
+}
+
+func runPractice2() {
+	collection := practice2.Collection{};
+	for i := 0; i < 10; i++ {
+		collection.Add(practice2.Element(i));
+	}
+	collection.Get(5);
+	if _, error := collection.Get(5); error != nil {
+		panic("There is no element with required index")
+	}
+	collection.Remove(5);
+	fmt.Printf("First: %v\n", collection.First());
+	fmt.Printf("Last: %v\n", collection.Last());
+	fmt.Printf("Curent: %v\n", collection.Value());
+	fmt.Printf("Next: %v\n", collection.Next());
+	fmt.Printf("Next: %v\n", collection.Next());
+	fmt.Printf("Prev: %v\n", collection.Prev());
+	fmt.Printf("Length: %v\n", collection.Length());
+	collection.Print();
+}
+
+func main() {
+	runPractice2();
 }
