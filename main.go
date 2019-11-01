07b27affeca553358ac4b6bdfa066348e382bfe6
Denis Autushenka <dautushenka@tula.co>
2019-11-01 17:58:36 +0300

    Implement interface for bot

--- main.go
+++ main.go
@@ -10,59 +10,58 @@ import (
 
 func runPractice1() {
 	// Practice 1
-	practice1.Fillarray(10, "0");
-	a := practice1.Filldiagonal(5, "1", "0");
+	practice1.Fillarray(10, "0")
+	a := practice1.Filldiagonal(5, "1", "0")
 	fmt.Println("[Practice 1] Print 2d array with filled dialonals")
-	practice1.Print2darray(a);
+	practice1.Print2darray(a)
 
 	fmt.Println("")
 	// Practice 1.2
 	fmt.Println("[Practice 1.2] Print dice statistic")
-	practice1.Dicestatistic(10000);
+	practice1.Dicestatistic(10000)
 }
 
 func runPractice2() {
-	collection := practice2.Collection{};
+	collection := practice2.Collection{}
 	for i := 0; i < 10; i++ {
-		collection.Add(practice2.Element(i));
+		collection.Add(practice2.Element(i))
 	}
-	collection.Get(5);
+	collection.Get(5)
 	if _, error := collection.Get(5); error != nil {
 		panic("There is no element with required index")
 	}
-	collection.Remove(5);
-	fmt.Printf("First: %v\n", collection.First());
-	fmt.Printf("Last: %v\n", collection.Last());
-	fmt.Printf("Curent: %v\n", collection.Value());
-	fmt.Printf("Next: %v\n", collection.Next());
-	fmt.Printf("Next: %v\n", collection.Next());
-	fmt.Printf("Prev: %v\n", collection.Prev());
-	fmt.Printf("Length: %v\n", collection.Length());
-	collection.Print();
+	collection.Remove(5)
+	fmt.Printf("First: %v\n", collection.First())
+	fmt.Printf("Last: %v\n", collection.Last())
+	fmt.Printf("Curent: %v\n", collection.Value())
+	fmt.Printf("Next: %v\n", collection.Next())
+	fmt.Printf("Next: %v\n", collection.Next())
+	fmt.Printf("Prev: %v\n", collection.Prev())
+	fmt.Printf("Length: %v\n", collection.Length())
+	collection.Print()
 }
 
 func runPractice2_1() {
-	collection := practice2_1.Collection{};
+	collection := practice2_1.Collection{}
 	for i := 0; i < 10; i++ {
-		collection.Add(i);
+		collection.Add(i)
 	}
-	collection.Print();
-	collection.Get(5);
+	collection.Print()
+	collection.Get(5)
 	if _, error := collection.Get(5); error != nil {
 		panic("There is no element with required index")
 	}
-	collection.Remove(5);
-	fmt.Println(collection.First());
-	fmt.Println(collection.Last());
-	fmt.Println(collection.Value());
-	fmt.Println(collection.Next());
-	fmt.Println(collection.Next());
-	fmt.Println(collection.Prev());
-	fmt.Println(collection.Length());
-	collection.Print();
+	collection.Remove(5)
+	fmt.Println(collection.First())
+	fmt.Println(collection.Last())
+	fmt.Println(collection.Value())
+	fmt.Println(collection.Next())
+	fmt.Println(collection.Next())
+	fmt.Println(collection.Prev())
+	fmt.Println(collection.Length())
+	collection.Print()
 }
 
-
 func main() {
-	practice3.RunBot2();
+	practice3.RunBot3()
 }
--- practice3/bot2.go
+++ practice3/bot2.go
@@ -2,16 +2,16 @@ package practice3
 
 import (
 	"fmt"
-	"time"
-	"strconv"
 	"os"
+	"strconv"
 	"strings"
+	"time"
 )
 
-var loc *time.Location;
+var loc *time.Location
 
 func init() {
-	loc, _ = time.LoadLocation("Europe/Minsk");
+	loc, _ = time.LoadLocation("Europe/Minsk")
 }
 
 type Bot struct {
@@ -20,60 +20,71 @@ type Bot struct {
 }
 
 func RunBot2() {
-	var input string;
-	bot := CreateBot(chooselang());
-	bot.printOptionList();
+	var input string
+	bot := CreateBot(chooselang())
+	bot.printOptionList()
 	for {
 		if _, err := fmt.Scanln(&input); err != nil {
-			input = "";
+			input = ""
 		}
-		input = strings.ToLower(input);
+		input = strings.ToLower(input)
 		if bot.checkExitCommand(input) {
-			bot.sayBye();
-			break;
+			bot.sayBye()
+			break
 		}
-		bot.ExecuteCommand(input);
+		bot.ExecuteCommand(input)
 	}
-	os.Exit(0);
+	os.Exit(0)
 }
 
 func CreateBot(lang string) Bot {
-	copy := GetCopy(lang);
+	copy := GetCopy(lang)
 
-	return Bot{copy("botName"), copy};
+	return Bot{copy("botName"), copy}
 }
 
 func (s *Bot) printOptionList() {
-	fmt.Println(s.copy("introduction"));
+	fmt.Println(s.copy("introduction"))
 	for i := 1; i <= 5; i++ {
-		fmt.Printf("%d. %s\n", i, s.copy("option" + strconv.Itoa(i)));
+		fmt.Printf("%d. %s\n", i, s.copy("option"+strconv.Itoa(i)))
 	}
 }
 
 func (s *Bot) checkExitCommand(command string) bool {
 	switch command {
-		default: return false;
-		case "5": fallthrough;
-		case s.copy("command5"): return true;
+	default:
+		return false
+	case "5":
+		fallthrough
+	case s.copy("command5"):
+		return true
 	}
 }
 
 func (s *Bot) ExecuteCommand(command string) {
 	switch command {
-		case "1": fallthrough;
-		case s.copy("command1"): s.sayHello(); 
-			break;
-		case "2": fallthrough;
-		case s.copy("command2"): s.sayTime();
-			break;
-		case "3": fallthrough;
-		case s.copy("command3"): s.sayDate();
-			break;
-		case "4": fallthrough;
-		case s.copy("command4"): s.sayDay();
-			break;
-		default: 
-			fmt.Println(s.copy("unknownCmd"));
+	case "1":
+		fallthrough
+	case s.copy("command1"):
+		s.sayHello()
+		break
+	case "2":
+		fallthrough
+	case s.copy("command2"):
+		s.sayTime()
+		break
+	case "3":
+		fallthrough
+	case s.copy("command3"):
+		s.sayDate()
+		break
+	case "4":
+		fallthrough
+	case s.copy("command4"):
+		s.sayDay()
+		break
+	default:
+		fmt.Println(s.copy("unknownCmd"))
 	}
 }
 
@@ -87,17 +98,21 @@ func (s *Bot) sayBye() {
 
 func (s *Bot) sayTime() {
 	t := time.Now().In(loc)
-	fmt.Println(s.copy("time", t.Format("15:04")));
+	fmt.Println(s.copy("time", t.Format("15:04")))
 }
-  
+
 func (s *Bot) sayDate() {
 	t := time.Now().In(loc)
 
-	fmt.Println(s.copy("date", t.Format("01-02-2006")));
+	fmt.Println(s.copy("date", t.Format("01-02-2006")))
 }
 
 func (s *Bot) sayDay() {
-	t := time.Now().In(loc);
+	t := time.Now().In(loc)
 
-	fmt.Println(s.copy("day", s.copy(t.Format("Monday"))));
+	fmt.Println(s.copy("day", s.copy(t.Format("Monday"))))
+}
+
+func (s *Bot) t(key string, args ...string) string {
+	return s.copy(key, args...)
 }
